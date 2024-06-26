50b7276db54835b3b20d63258408b4ad3bed9b5d
mary <impillimariarosaria@gmail.com>
2024-06-26 15:57:27 +0200

    init

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,89 @@
+const Booking = require("../models/bookingModel");
+
+const getBookings = async (req, res, next) => {
+  try {
+    const bookings = await Booking.find();
+    if (!bookings) {
+      res.status(400);
+      throw new Error("Non riesco a trovare la prenotazione");
+    }
+
+    return res.status(200).json(bookings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// create booking
+const createBooking = async (req, res, next) => {
+  try {
+    const booking = await Booking.create(req.body);
+    if (!booking) {
+      res.status(400);
+      throw new Error("Non riesco a creare la prenotazione");
+    }
+
+    return res.status(201).json(booking);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateBooking = async (req, res, next) => {
+  try {
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedBooking) {
+      res.status(400);
+      throw new Error("Non posso creare la prenotazione");
+    }
+    const bookings = await Booking.find();
+    return res.status(200).json(bookings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteBooking = async (req, res, next) => {
+  try {
+    const room = await Booking.findByIdAndDelete(req.params.id);
+    if (!room) {
+      res.status(400);
+      throw new Error("Non si può cancellare la prenotazione");
+    }
+    return res.status(200).json({ id: req.params.id });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//get singel booking
+const getBooking = async (req, res, next) => {
+  try {
+    const booking = await Booking.findById(req.params.id);
+    if (!booking) {
+      res.status(400);
+      throw new Error("Prenotazione non trovata");
+    }
+
+    return res.status(200).json(booking);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getBookings,
+  createBooking,
+  updateBooking,
+  deleteBooking,
+  getBooking,
+};
---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+const {
+  getBookings,
+  createBooking,
+  updateBooking,
+  deleteBooking,
+  getBooking,
+} = require("../controllers/bookingController");
+
+const router = Router();
+
+// get booking
+router.get("/", getBookings);
+
+// get single booking
+router.get("/:id", getBooking);
+
+// create booking
+router.post("/", createBooking);
+
+// update booking
+router.put("/:id", updateBooking);
+
+//delete booking
+router.delete("/:id", deleteBooking);
+
+module.exports = router;
